8465a36900e7c8253c83853134957515758ebfeb
ahmedrashraf <ahmedr.0331@gmail.com>
2025-08-07 16:33:20 +0500

    useTranslation

--- lib/useTranslation.ts
+++ lib/useTranslation.ts
@@ -1,34 +1,24 @@
 'use client';
 
-import en from "@/public/locales/en";
+import en from "@/public/locales/en"
 import de from '@/public/locales/de';
-import { useEffect, useState } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 
 const translations = { en, de };
 
 export default function useTranslation() {
+  const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
 
-  const [lang, setLang] = useState<'en' | 'de'>('en');
-
-  useEffect(() => {
-    const storedLang = localStorage.getItem('lang') as 'en' | 'de' | null;
-    if (storedLang) {
-      setLang(storedLang);
-    } else {
-      localStorage.setItem('lang', 'en'); // default to English
-    }
-  }, []);
+  const lang = (searchParams.get('lang') as 'en' | 'de') || 'en';
 
   const t = translations[lang];
 
   const switchLanguage = () => {
     const newLang = lang === 'en' ? 'de' : 'en';
-    setLang(newLang);
-    localStorage.setItem('lang', newLang);
-    router.push(pathname); // Reload without ?lang query
+    const url = `${pathname}?lang=${newLang}`;
+    router.push(url);
   };
 
   return { t, lang, switchLanguage };
